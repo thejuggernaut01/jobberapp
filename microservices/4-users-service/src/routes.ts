797d1c5cf73df239ea305d5b652d3c4fa71f0b97
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-11-24 06:25:34 +0100

    chore: seller create, update, and get controller

--- microservices/4-users-service/src/routes.ts
+++ microservices/4-users-service/src/routes.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import { buyerRoutes } from '@users/routes/buyer.routes';
 import { healthRoutes } from '@users/routes/health.routes';
+import { sellerRoutes } from '@users/routes/seller.routes';
 
 const BUYER_BASE_PATH = '/api/v1/buyer';
 const SELLER_BASE_PATH = '/api/v1/seller';
@@ -8,7 +9,7 @@ const SELLER_BASE_PATH = '/api/v1/seller';
 const appRoutes = (app: Application): void => {
   app.use('', healthRoutes());
   app.use(BUYER_BASE_PATH, buyerRoutes());
-  app.use(SELLER_BASE_PATH, () => console.log('seller routes'));
+  app.use(SELLER_BASE_PATH, sellerRoutes());
 };
 
 export { appRoutes };
